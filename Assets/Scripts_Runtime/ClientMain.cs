fdf59a98b8b8c3c3aa100c090ea4d7469fb10916
Jackutea <chenwansal1@163.com>
2024-11-13 20:56:26 +0800

    <feature> add: 连接至服务端

---
+++ Assets/Scripts_Runtime/ClientMain.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Telepathy;
+
+namespace UnityClient {
+
+    public class ClientMain : MonoBehaviour {
+
+        Telepathy.Client client;
+
+        void Start() {
+            int messageSize = 1024;
+            client = new Client(messageSize);
+
+            // - Binding Events
+            client.OnConnected = () => {
+                Debug.Log("Connected to the server");
+            };
+
+            client.OnData = (ArraySegment<byte> data) => {
+                Debug.Log("Received a message from the server");
+            };
+
+            client.OnDisconnected = () => {
+                Debug.Log("Disconnected from the server");
+            };
+
+            client.Connect("localhost", 7777);
+
+            Debug.Log("Press Space to connect/disconnect");
+
+        }
+
+        void Update() {
+
+            if (Input.GetKeyUp(KeyCode.Space)) {
+                if (client.Connected) {
+                    client.Send(new ArraySegment<byte>(new byte[1] { 0 }));
+                    Debug.Log("Connecting to the server");
+                }
+            }
+
+            client.Tick(100);
+
+        }
+    }
+}
